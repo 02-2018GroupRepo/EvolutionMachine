cc9686f729e13842e6afdc4e36febf9e8fc57aa0
syntel <nadario.seays@gmail.com>
2018-03-21 17:41:56 -0400

    changes main

--- src/main/java/hello/Main.java
+++ src/main/java/hello/Main.java
@@ -1,8 +1,23 @@
 package hello;
 
-import java.util.Scanner;
 
 public class Main {
 
+    public static void main (String [] args) {
 
+        Product kitkat = new Product("KitKat", 1, 1.50, 0.90, "Candy");
+        Product twix = new Product("Twix", 2, 1.70, 1.20, "Candy");
+
+        Machine vendingMachine = new Machine();
+        vendingMachine.addItemToMachine(kitkat, "C6", 6);
+       //vendingMachine.addItemToMachine(twix, "A2", 6);
+
+//        vendingMachine.removeItem();
+//
+//        vendingMachine.viewCoinCart();
+//        vendingMachine.getSizeOfCompt("A1");
+//        vendingMachine.getSizeOfCompt("A2");
+
+
+    }
 }
