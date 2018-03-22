c51ff1945b3d751c6bbf27f69353f31fa0016630
knosence <nadario.seays@gmail.com>
2018-03-22 13:03:04 -0400

    Added scanner for customer to insert coins,changed 1 method in coin manager class, added commments and todos in code

--- src/main/java/hello/CoinManager.java
+++ src/main/java/hello/CoinManager.java
@@ -35,7 +35,7 @@ public class CoinManager {
     public boolean hasEnoughForProduct(Product product){
         boolean confirmed = false;
 
-        if (customerTotalInputAmount() >= product.retailPrice){
+        if (customerTotalInputAmount() >= product.getRetailPrice()){
              confirmed = true;
         }
 
--- src/main/java/hello/Main.java
+++ src/main/java/hello/Main.java
@@ -1,6 +1,10 @@
 package hello;
 
 
+import com.sun.xml.internal.bind.v2.TODO;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main (String [] args) {
@@ -17,6 +21,60 @@ public class Main {
 //        vendingMachine.viewCoinCart();
 //        vendingMachine.getSizeOfCompt("A1");
 //        vendingMachine.getSizeOfCompt("A2");
+
+        Scanner scan = new Scanner(System.in);
+        CoinManager coinManager = new CoinManager();
+
+        boolean switchOut = true;
+
+        while(switchOut) {
+
+        System.out.println("\nEnter numbers 1 - 4 for the coin that you would like to insert");
+        System.out.printf("You currently have $%.2f inserted%n", coinManager.customerTotalInputAmount());
+        System.out.println("1: Quarter \n2: Dime \n3: Nickle \n4: I'm done entering coins. Product please!");
+
+        int userInput = scan.nextInt();
+        switch(userInput) {
+
+            case 1:
+
+                coinManager.addCustomerCoins("quarter");
+                break;
+
+            case 2:
+
+                coinManager.addCustomerCoins("dime");
+                break;
+
+            case 3:
+
+                coinManager.addCustomerCoins("nickel");
+                break;
+
+            case 4:
+
+                switchOut = false;
+                break;
+
+            default:
+
+                System.out.println("Seriously? It's just four choices");
+
+        }
+            System.out.printf("You currently have $%.2f inserted%n", coinManager.customerTotalInputAmount());
+            //TODO: Select product method
+
+            /**
+             * use this method to confirm that the customer has enough for product
+             * coinManager.hasEnoughForProduct(Product product)
+             *
+             * if true, when the product is dispensed, use this method to add customer coins to coin manager
+             * coinManager.addCustomerCoinToCM()
+             */
+            //TODO: If false, return customer coins method
+
+        }
+
 
 
     }
