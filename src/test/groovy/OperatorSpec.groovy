f55f494b8f0a4ea186090f596bac3349b45088dd
Darkblader714 <MarioL714@gmail.com>
2018-03-23 16:40:13 -0400

    The Operator is now able to get the total cash amount and the total number of each coin in each machine

--- src/test/groovy/OperatorSpec.groovy
+++ src/test/groovy/OperatorSpec.groovy
@@ -3,7 +3,7 @@ import spock.lang.Specification
 
 class OperatorSpec extends Specification{
 
-    def "Get total amounts of money and coins"(){
+    def "Get total amount of money"(){
 
         given: "A machine with a total of 12 dollars";
 
@@ -16,9 +16,34 @@ class OperatorSpec extends Specification{
         double moneyTotalAmount = dM.getTotalMoneyValue();
 
 
-        then: "the operator is able to view the total amount and individual coins";
+        then: "the operator is able to view the total amount of cash";
 
         moneyTotalAmount==12;
+
+    }
+
+    def "Get total amount of coins"(){
+
+        given: "A Machine";
+
+        DrinkMachine dM = new DrinkMachine("Smyrna", "GA", "001")
+
+        dM.setQuarterCount(5);
+        dM.setDimeCount(5);
+        dM.setNickelCount(5);
+
+        when: "an operator accesses a machine";
+
+        double quarterTotalAmount = dM.getQuarterCount()
+        double dimeTotalAmount = dM.getDimeCount()
+        double nickelTotalAmount = dM.getNickelCount()
+
+        then: "the operator is able to view the total individual coins"
+
+        quarterTotalAmount==5;
+        dimeTotalAmount==5;
+        nickelTotalAmount==5;
+
 
     }
 
