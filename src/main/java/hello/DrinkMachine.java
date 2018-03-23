ccb874a5bca77d7080536694bc91f76bcfba40f6
Mayur Maisuria <mickmai26@gmail.com>
2018-03-23 09:45:03 -0400

    "Latest version of DrinkMachine (Removed all the comment)"

--- src/main/java/hello/DrinkMachine.java
+++ src/main/java/hello/DrinkMachine.java
@@ -8,14 +8,9 @@ import java.util.*;
  */
 public class DrinkMachine {
 
-//        String company = "Thank you for using The Evolution Machine!";
-//        System.out.println(company);
-
     private String name;
 
     Map<String, Queue<Product>> mapComtProd;
-
-   // Map<Double, Integer> coinCart;
     Scanner input = new Scanner(System.in);
 
     CoinManager drinkCoinManager;
@@ -23,7 +18,6 @@ public class DrinkMachine {
     public DrinkMachine() {
         drinkCoinManager = new CoinManager();
         mapComtProd = new HashMap<>();
-        //coinCart = new HashMap<>();
     }
 
     public void setName(String name) {
@@ -127,117 +121,6 @@ public class DrinkMachine {
                 String change = drinkCoinManager.returnCustomerChangeInserted();
                 System.out.println(change);
             }
-//            mapComtProd.get(index);
-
-            /*
-             *  From line number 114 to 217, I need functions from Nadario and Amber
-             *  From line number 117 to 214, I need functions from Brandon and Mariano
-             */
-
-//            while(!done) {
-//
-//                System.out.println("Please insert the coins \n 1) $0.25 \n 2) $0.10 \n 3) $0.05 \n 4) Exit \n");
-//                option = input.nextInt();
-//
-//                switch (option) {
-//
-//                    case 1:
-//                        System.out.println("Enter the number of coins $0.25");
-//                        numOfQut = input.nextInt();
-//                        if(coinCart.get(0.25) != null) {
-//                            int quarter = coinCart.get(0.25);
-//                            coinCart.put(0.25, quarter + numOfQut);
-//                        } else {
-//                            coinCart.put(0.25, numOfQut);
-//                        }
-//                        break;
-//
-//                    case 2:
-//                        System.out.println("Enter the number of coins $0.10");
-//                        numOfDim = input.nextInt();
-//                        if(coinCart.get(0.10) != null) {
-//                            int dime = coinCart.get(0.10);
-//                            coinCart.put(0.10, dime + numOfDim);
-//                        } else {
-//                            coinCart.put(0.10, numOfDim);
-//                        }
-//                        break;
-//
-//                    case 3:
-//                        System.out.println("Enter the number of coins $0.05");
-//                        numOfNick = input.nextInt();
-//                        if(coinCart.get(0.05) != null) {
-//                            int nickle = coinCart.get(0.05);
-//                            coinCart.put(0.05, numOfNick + nickle);
-//                        } else {
-//                            coinCart.put(0.05, numOfNick);
-//                        }
-//                        break;
-//
-//                    case 4:
-//                        System.out.println("Done inserting the coins");
-//                        done = true;
-//                        break;
-//                }
-//            }
-//
-//            Queue<Product> queOfProd = mapComtProd.get(index);
-//            double total = (numOfQut * 0.25) + (numOfDim * 0.10) + (numOfNick * 0.05);
-//            total = Double.parseDouble(String.format("%.2f", total));
-//            double changeBack = total - (queOfProd.peek().retailPrice);
-//            changeBack = Double.parseDouble(String.format("%.2f", changeBack));
-//            double returnCoins = Double.parseDouble(String.format("%.2f", changeBack));
-//
-//            System.out.println("You have inserted " + total);
-//            System.out.println("Item price is " + queOfProd.peek().retailPrice);
-//
-//            if(total >= queOfProd.peek().retailPrice)      {
-//                System.out.println("Please collect your item " + queOfProd.peek().getName());
-//                queOfProd.remove();
-//                mapComtProd.put(index, queOfProd);
-//
-//                int changeQt = 0;
-//                int changeDm = 0;
-//                int changeNic = 0;
-//                boolean zero = false;
-//
-//                while(!zero) {
-//                    if((changeBack - 0.25) >= 0.0) {
-//                        changeQt++;
-//                        changeBack = (changeBack - 0.25);
-//                        changeBack = Double.parseDouble(String.format("%.2f", changeBack));
-//                    } else if ((changeBack - 0.10) >= 0.0) {
-//                        changeDm++;
-//                        changeBack = (changeBack - 0.10);
-//                        changeBack = Double.parseDouble(String.format("%.2f", changeBack));
-//                    } else if ((changeBack - 0.05) >= 0.0) {
-//                        changeNic++;
-//                        changeBack = (changeBack - 0.05);
-//                        changeBack = Double.parseDouble(String.format("%.2f", changeBack));
-//                    } else {
-//                        zero = true;
-//                    }
-//                }
-//                if(coinCart.get(0.25) != null) {
-//                    coinCart.put(0.25, (coinCart.get(0.25) - changeQt));
-//                }
-//                if(coinCart.get(0.10) != null) {
-//                    coinCart.put(0.10, (coinCart.get(0.10) - changeDm));
-//                }
-//                if(coinCart.get(0.05) != null) {
-//                    coinCart.put(0.05, (coinCart.get(0.05) - changeNic));
-//                }
-//
-//                System.out.println("Please collect your chnage " + returnCoins);
-//                System.out.println("Please collect your coins back $0.25 :" + changeQt + " $0.10 :" + changeDm + " $0.05 : " + changeNic);
-//
-//            } else {
-//                System.out.println("Please enter sufficient fund");
-//                System.out.println("Please collect your coins back $0.25 - " + numOfQut + " $0.10 - " + numOfDim + " $0.05 - " + numOfNick);
-//                coinCart.put(0.25, coinCart.get(0.25) - numOfQut);
-//                coinCart.put(0.10, coinCart.get(0.10) - numOfDim);
-//                coinCart.put(0.05, coinCart.get(0.05) - numOfNick);
-//            }
         }
     }
 
